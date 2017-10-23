d07eb4770f91677795e7bcfdc02076f2f2c8fbcf
esdes-gatti-clara <32122407+esdes-gatti-clara@users.noreply.github.com>
2017-10-23 11:51:28 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+console.log(document);
+
+var name ="clara";
+
+function changeName (newName) {
+    name = newName;
+    alert(name);
+}
+
+function changeClass(){
+    var button = document.getElementById("myButton");
+    if(button.className=="rotate"){
+    button.className="";
+    
+    } else{
+        button.className = "rotate"; 
+    }
+}
